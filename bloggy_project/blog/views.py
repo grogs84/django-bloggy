07e9e44876b9e57839f8143249ad1981f9e5efde
Matt Grogan <johnmattgrogan@gmail.com>
2015-08-31 16:25:06 -0500

    updated popular posts. added boostrap gird system

--- bloggy_project/blog/views.py
+++ bloggy_project/blog/views.py
@@ -6,20 +6,37 @@ from django.template import Context, loader
 
 from blog.models import Post
 
+### helper functions ####
+def encode_url(url):
+	return url.replace(' ', '_')
 
+def get_popular_posts():
+	posts = Post.objects.order_by('-views')[:5]
+	for post in posts:
+		post.url = encode_url(post.title)
+	return posts
+
+### views ####
 def index(request):
 	latest_posts  = Post.objects.all().order_by('-created_at')
 	t = loader.get_template('blog/index.html')
-	context_dict = {'latest_posts': latest_posts,}
+	context_dict = {
+		'latest_posts': latest_posts,
+		'popular_posts': get_popular_posts(),
+		}
 	for post in latest_posts:
-		post.url = post.title.replace(' ', '_')
-	c = Context({'latest_posts': latest_posts, })
+		post.url = encode_url(post.title)
+	c = Context(context_dict)
 	return HttpResponse(t.render(c))
 
 def post(request, post_url):
 	single_post = get_object_or_404(Post, title=post_url.replace('_', ' '))
 	single_post.views += 1
 	single_post.save()
+	context_dict = {
+		'single_post': single_post,
+		'popular_posts': get_popular_posts(),
+	}
 	t = loader.get_template('blog/post.html')
-	c = Context({'single_post': single_post })
+	c = Context(context_dict)
 	return HttpResponse(t.render(c))
